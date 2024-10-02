faf1351c94a1fad0b65014b3f2284550ac0f134d
eliseograncelli <eliseograncelli@gmail.com>
2024-10-02 11:13:43 -0300

    Cambie funcionamiento a la hora de preguntar riesgos.

--- main.pl
+++ main.pl
@@ -1,24 +1,33 @@
 
-riesgo("Criptomonedas",alto).
-riesgo("Acciones",alto).
-riesgo("Bonos del Tesoro", medio).
-riesgo("Comparar Dolares", bajo).
-riesgo("Fondos Mutuos",medio).
-riesgo("Cuentas de Ahorro",bajo).
+
+:- dynamic(riesgo/2).
+:- dynamic(inveriones/3).
+:- dynamic(riesgoUsuario/2).
 
 
-inversion_minima("Acciones", 1).
-inversion_minima("Bonos del Tesoro", 100).
-inversion_minima("Comparar Dolares", 100).
-inversion_minima("Fondos Mutuos", 500).
-inversion_minima("Cuentas de Ahorro", 1).
+clearScreen :-
+	write('\e[H\e[2J').
+
+cargarConocimiento :-
+	retractall(riesgo(_,_)),retractall(inversiones(_,_,_)),
+	retractall(riesgo_usuario(_,_)), 
+	consult('/home/eliseo/Escritorio/IA/tpi-ia/bd_riesgos.pl'),consult('/home/eliseo/Escritorio/IA/tpi-ia/bd_inversiones.pl').
 
 
 
-preguntar_riesgo(Decision) :-
-    writeln("Que riesgo estas dispuesto a asumir? (bajo, medio, alto)"),
-    read(Decision)
-.
+validar_riesgo:- riesgo(RiesgoID,Medida), write('Estas dispuesto a asumir un riesgo '), write(Medida), write(' en tu inverison? [s/n]: '),
+ read(Decision), validar_desicion(RiesgoID,Medida,Decision), validar_riesgo.
+
+validar_riesgo:- riesgo_usuario(_,_), writeln('Ok! Ahora que se el riesgo que estas dispuesto a asumir continuemos con otras preguntas.').
+
+validar_riesgo:- not(riesgo_usuario(_,_)), writeln('Lamento no poder ayudarte, todas las inversiones disponibles implican un minimo de riesgo.').
+
+validar_desicion(RiesgoID,Medida,Decision):-Decision='s', retract(riesgo(RiesgoID,_)), assert(riesgo_usuario(RiesgoID,Medida)).
+
+validar_desicion(RiesgoID,_,Desicion):- Desicion='n', retract(riesgo(RiesgoID,_)), retractall(inversiones(_,RiesgoID,_)).
+
+validar_desicion(_,_,_).
+
 
 preguntar_monto(Decision) :-
     writeln("Cuanto estas dispuesto a invertir? (monto en dolares)"),
@@ -56,8 +65,6 @@ listar_recomendaciones([H | T]):-
 
 listar_recomendaciones([]).
 
-main:- 
-  preguntar_riesgo(Riesgo),
-  preguntar_monto(Monto),
-  recomendar_inversion(Riesgo, Monto),
-  nl.
+
+
+recomendame:-cargarConocimiento,validar_riesgo.
